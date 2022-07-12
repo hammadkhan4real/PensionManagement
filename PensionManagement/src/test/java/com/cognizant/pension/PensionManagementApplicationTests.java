4ef55bf90acb272abcb9506b5d6094d2070b94ff
893949 <893949@LTIN287809.cts.com>
2022-07-12 17:46:19 +0530

    first commit

---
+++ PensionManagement/src/test/java/com/cognizant/pension/PensionManagementApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cognizant.pension;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PensionManagementApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
